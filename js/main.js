939ab3e30477443dd1eb77c9ca7e30351465bdf0
Fedor Sharaputo <sharaputo@icloud.com>
2022-09-02 10:12:03 +0300

    first version

---
+++ js/main.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const passwordInput = document.getElementById("password");
+  const genBtn = document.getElementById("generate");
+  const copyBtn = document.getElementById("copy");
+
+  function genPassword() {
+    const chars =
+      "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const passwordLength = 12;
+    let password = "";
+
+    for (let i = 0; i < passwordLength; i++) {
+      let randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+      passwordInput.value = password;
+    }
+  }
+
+  function copyPassword() {
+    passwordInput.select();
+    navigator.clipboard.writeText(passwordInput.value);
+  }
+
+  genBtn.addEventListener("click", genPassword);
+  copyBtn.addEventListener("click", copyPassword);
+});
