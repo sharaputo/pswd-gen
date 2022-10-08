6f584283fa02e0c8dec20d74cdb32cea8232306c
Fedor Sharaputo <sharaputo@icloud.com>
2022-10-08 15:36:53 +0300

    pwd length option added

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const passwordInput = document.getElementById("password");
-  const genBtn = document.getElementById("generate");
-  const copyBtn = document.getElementById("copy");
+window.addEventListener('DOMContentLoaded', () => {
+  const radios = document.querySelectorAll('.radio');
+  const pswLength = document.getElementById('length');
+  const passwordInput = document.getElementById('password');
+  const genBtn = document.getElementById('generate');
+  const copyBtn = document.getElementById('copy');
+
+  let passwordLength = 12;
+
+  radios.forEach((radio) => {
+    radio.addEventListener('change', (event) => {
+      passwordLength = Number.parseInt(event.target.value);
+      pswLength.textContent = `(password length is ${passwordLength})`;
+    });
+  });
 
   function genPassword() {
     const chars =
-      "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      '0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    const passwordLength = 12;
-    let password = "";
+    let password = '';
 
     for (let i = 0; i < passwordLength; i++) {
       let randomNumber = Math.floor(Math.random() * chars.length);
@@ -16,27 +26,27 @@ window.addEventListener("DOMContentLoaded", () => {
       passwordInput.value = password;
     }
 
-    genBtn.innerHTML = "Generated";
-    genBtn.classList.add("_active");
+    genBtn.innerHTML = 'Generated';
+    genBtn.classList.add('_active');
 
     setTimeout(() => {
-      genBtn.innerHTML = "Generate";
-      genBtn.classList.remove("_active");
+      genBtn.innerHTML = 'Generate';
+      genBtn.classList.remove('_active');
     }, 500);
   }
 
   function copyPassword() {
     passwordInput.select();
     navigator.clipboard.writeText(passwordInput.value);
-    copyBtn.innerHTML = "Copied";
-    copyBtn.classList.add("_active");
+    copyBtn.innerHTML = 'Copied';
+    copyBtn.classList.add('_active');
 
     setTimeout(() => {
-      copyBtn.innerHTML = "Copy";
-      copyBtn.classList.remove("_active");
+      copyBtn.innerHTML = 'Copy';
+      copyBtn.classList.remove('_active');
     }, 500);
   }
 
-  genBtn.addEventListener("click", genPassword);
-  copyBtn.addEventListener("click", copyPassword);
+  genBtn.addEventListener('click', genPassword);
+  copyBtn.addEventListener('click', copyPassword);
 });
